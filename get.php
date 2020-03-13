f2fbc6ec7416f8573f72c6f80008a70dab445809
tohoohome <62142195+tohoohome@users.noreply.github.com>
2020-03-13 15:37:17 +0200

    Create get.php

---
+++ get.php
@@ -0,0 +1,20 @@
+#EXTM3U
+#EXTINF:-1,BeIN 1
+https://www.ostora-tv.com/live/20831835/1544487/70134
+#EXTINF:-1,BeIN 2
+https://www.ostora-tv.com/live/20831835/1544487/70135
+#EXTINF:-1,BeIN 3
+https://www.ostora-tv.com/live/20831836/1544487/70136
+#EXTINF:-1,BeIN 4
+https://www.ostora-tv.com/live/20831836/1544487/70137
+#EXTINF:-1,BeIN 5
+https://www.ostora-tv.com/live/20831836/1544487/70138
+#EXTINF:-1,BeIN 6
+https://www.ostora-tv.com/live/20831836/1544487/70139
+#EXTINF:-1,BeIN 7
+https://www.ostora-tv.com/live/20831836/1544487/70140
+#EXTINF:-1,BeIN 8
+https://www.ostora-tv.com/live/20831836/1544487/80874
+#EXTINF:-1,BeIN 9
+https://www.ostora-tv.com/live/20831836/1544487/110500
+© 2020 GitHub, Inc.
